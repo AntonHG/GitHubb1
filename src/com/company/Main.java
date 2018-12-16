5c381cd1f21a2a8aa37675c837417d3a012f977d
AntonHG <xlangell@mail.ru>
2018-12-16 18:29:47 +0200

    Hello Hello!!1 was changed "Hello QAHaifa!2"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello QAHaifa-1!!111");
+	System.out.println("Hello QAHaifa-1");
     }
 }
